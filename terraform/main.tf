3850745adc8078e9c6e816535742e9b9a02c4341
Fenglin Han <fenglin.han@telia.no>
2019-01-23 09:16:27 +0100

    attach policy to role

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,25 @@ resource "aws_iam_role" "lambda_role" {
   name = "${var.name_prefix}_lambda_role"
 
   assume_role_policy = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+EOF
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "${var.name_prefix}_lambda_role"
+
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -31,17 +50,20 @@ resource "aws_iam_role" "lambda_role" {
                 "s3:GetObject"
             ],
             "Resource": "arn:aws:s3:::${var.db_logs_state_bucket_name}/*"
-        },
-        {
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
         }
     ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = "${aws_iam_role.lambda_role.name}"
+  policy_arn = "${aws_iam_policy.lambda_policy.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-policy-attachment" {
+  role       = "${aws_iam_role.lambda_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
 data "archive_file" "lambda_zip" {
