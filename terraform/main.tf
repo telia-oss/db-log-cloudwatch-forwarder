32ac4ea474301c69467ace04aa4981225b09101b
Fenglin Han <fenglin.han@telia.no>
2019-01-23 09:26:26 +0100

    update lambda module tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,18 @@ resource "aws_iam_role" "lambda_role" {
   name = "${var.name_prefix}_lambda_role"
 
   assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
-        }
-      ]
-    }
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "lambda.amazonaws.com"
+    },
+    "Action": "sts:AssumeRole"
+   }
+ ]
+}
 EOF
 }
 
